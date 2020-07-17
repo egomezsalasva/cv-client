7d62466f0bd2ee284fe94570053b3442e63314fd
Kike <kike@Kikes-MacBook-Pro.local>
2020-07-17 12:45:03 +0200

    import i18n to index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//IMPORTS
+//-Libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom"
+import './i18n' //-Multilanguage // import i18n (needs to be bundled ;))
+//-Styling
+import './index.css';
+//-Components
+import App from './App';
+//-Context API
 import  ThemeContextProvider  from './contexts/ThemeContext'
 
 ReactDOM.render(
