c352de94ea787020b68ee31e7863bbefcf038820
Kike <kike@Kikes-MacBook-Pro.local>
2020-07-29 12:36:15 +0200

    What Im Looking For

---
+++ src/components/cv/cvDeskComponents/rightPanel/sections/WhatImLookingFor.js
@@ -0,0 +1,42 @@
+//IMPORTS
+//-Modules
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+
+
+//MAIN COMPONENT
+const WhatImLookingForSection = () => {
+
+  //Translation
+    const { t } = useTranslation()
+  //
+    
+  return (
+    <div className="section" id="whatImLookingSection">
+
+        <div className="secInnerContainer">
+
+            <div className="storyText">
+                <p>{t("cv.whatImLookingForSection.story.p.1")}</p>
+                <p>{t("cv.whatImLookingForSection.story.p.2")}</p>
+                <p>{t("cv.whatImLookingForSection.story.p.3")}</p>
+                <p>{t("cv.whatImLookingForSection.story.p.4")}</p>
+                <p>{t("cv.whatImLookingForSection.story.p.5")}</p>
+            </div>
+
+            <div className="summaryText">
+                <ul>
+                    <li>{t("cv.whatImLookingForSection.summary.li.1")}</li>
+                    <li>{t("cv.whatImLookingForSection.summary.li.2")}</li>
+                    <li>{t("cv.whatImLookingForSection.summary.li.3")}</li>
+                    <li>{t("cv.whatImLookingForSection.summary.li.4")}</li>
+                </ul>
+            </div>
+            
+        </div>
+
+    </div> 
+  )
+}
+export default WhatImLookingForSection
